eef2f0bcd23f260036c502fba1a107b5b7bfd7a3
Jean-Eudes Ranvier <jeranvier@gmail.com>
2015-06-22 14:11:10 +0200

    add inverse fourier transform

--- src/jeranvier/math/dsp/Fourier.java
+++ src/jeranvier/math/dsp/Fourier.java
@@ -12,11 +12,27 @@ public class Fourier {
 	    	Complex sum = new Complex();
 	        for (int i = 1; i <= in.size(); i++) {
 	            double angle = 2 * Math.PI * i * o / size;
-	            Complex multiple = new Complex(Math.cos(angle), Math.sin(angle));
+	            Complex multiple = new Complex(Math.cos(angle), -1*Math.sin(angle));
 	            sum = sum.add(in.get(i).multiplyBy(multiple));
 
 	        }
 	        outBuilder.set(o, sum);
+        }
+	    return outBuilder.build();
+	}
+	
+	public static Vector ift(Vector in) {
+		Vector.Builder outBuilder = new Vector.Builder(in.size());
+		int size = in.size();
+	    for (int o = 1; o <= in.size(); o++) {
+	    	Complex sum = new Complex();
+	        for (int i = 1; i <= in.size(); i++) {
+	            double angle = 2 * Math.PI * i * o / size;
+	            Complex multiple = new Complex(Math.cos(angle), Math.sin(angle));
+	            sum = sum.add(in.get(i).multiplyBy(multiple));
+
+	        }
+	        outBuilder.set(o, sum.divideBy(size));
         }
 	    return outBuilder.build();
 	}
