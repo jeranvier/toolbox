c7677c17481e48727e92bf0f943794f4d05795b9
Jean-Eudes Ranvier <jeranvier@gmail.com>
2015-09-11 16:04:29 +0200

    the mysteries of git... apparently the file has changed, but could not figure out in which way...

--- src/jeranvier/math/dsp/ButterworthFilter.java
+++ src/jeranvier/math/dsp/ButterworthFilter.java
@@ -31,14 +31,12 @@ public class ButterworthFilter {
 		Vector.Builder filteredBuilder = new Vector.Builder(n);
 		filteredBuilder.set(1, fft.get(1));
 		filteredBuilder.set(n, fft.get(n));
-		
 		for(int i = 2; i <= (n+1)/2; i++){
 			double binFreq = binWidth * i;
 			double gain = Math.abs(DCGain)/(Math.sqrt( ( 1 + Math.pow( binFreq / frequencyCut, 2.0 * filterOrder))));
 			filteredBuilder.set(i, fft.get(i).multiplyBy(gain));
 			filteredBuilder.set(n-i+1, fft.get(n-i+1).multiplyBy(gain));
 		}
-		
 		Vector filtered = filteredBuilder.build();
 
 		return Fourier.ift(filtered);
