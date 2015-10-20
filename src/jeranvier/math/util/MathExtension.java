078cc6fd0a56f3aaab07af35f0080e4487a2ffbc
Jean-Eudes Ranvier <jeranvier@gmail.com>
2015-10-20 14:37:48 +0200

    add timeseries useful operations

--- src/jeranvier/math/util/MathExtension.java
+++ src/jeranvier/math/util/MathExtension.java
@@ -1,11 +1,15 @@
 package jeranvier.math.util;
 
+import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import jeranvier.math.timeseries.Timeseries;
 
 public class MathExtension {
 	
@@ -67,6 +71,26 @@ public class MathExtension {
 	public static float max(float... candidates){
 		Arrays.sort(candidates);
 		return candidates[candidates.length-1];
+	}
+	
+	public static Entry<Long, Double> max(Timeseries ts){
+		Entry<Long, Double> max = new AbstractMap.SimpleEntry<Long, Double>(-1l, Double.NEGATIVE_INFINITY);
+		for(Entry<Long, Double> entry : ts.entrySet()){
+			if(entry.getValue() > max.getValue()){
+				max = new AbstractMap.SimpleEntry<Long, Double>(entry);
+			}
+		}
+		return max;
+	}
+	
+	public static Entry<Long, Double> min(Timeseries ts){
+		Entry<Long, Double> min = new AbstractMap.SimpleEntry<Long, Double>(-1l, Double.POSITIVE_INFINITY);
+		for(Entry<Long, Double> entry : ts.entrySet()){
+			if(entry.getValue() < min.getValue()){
+				min = new AbstractMap.SimpleEntry<Long, Double>(entry);
+			}
+		}
+		return min;
 	}
 	
 }
